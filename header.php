546da40fed006f6fb01f8fe6b776dc5194fa5bc7
minkcv <minkcv@gmail.com>
2014-04-27 02:55:47 -0500

    added header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+echo '
+    <div id="menu">
+    <ul id="menulist">
+    <li> <a href="games.php">Games</a> </li>
+    <li> <a href="index.php">About</a> </li>
+    </ul>
+    </div>
+';
---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css"> 
+        
+        <title>Games and stuff</title>
+    </head>
+    <body>
+        <h1>Another Will Smith's Home Page</h1>
+        <?php include 'header.php'; ?>
+        <ul>
+            <li>
+                <h2>Large World</h2>
+                <img src="games/largeworld.PNG">
+                <p><a href="games/largeWorld.jar">Download</a></p>
+            </li>
+            <li>
+                <h2>Triangle Tetris</h2>
+                <img src="games/triangletetris.PNG">
+                <p><a href="games/TriangleTetris.jar">Download (Classic Version)</a></p>
+                <p><a href="games/TriangleTetris3.jar">Download (Expo Version)</a></p>
+            </li>
+            <li>
+                <h2>Space Run</h2>
+                <img src="games/spacerun.PNG">
+                <p><a href="games/spacerun.jar">Download</a></p>
+            </li>
+            <li>
+                <h2>Purple</h2>
+                <img src="games/purple.PNG">
+                <p><a href="games/purple.jar">Download</a></p>
+            </li>
+        </ul>
+        <script type="text/javascript">
+
+              var _gaq = _gaq || [];
+                _gaq.push(['_setAccount', 'UA-49640635-1']);
+                  _gaq.push(['_trackPageview']);
+
+                    (function() {
+                        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                                  })();
+
+                              </script>
+    </body>
+</html>
