bb0021ffa44c0dadd47ad6525b13258e3c501e26
minkcv <minkcv@gmail.com>
2014-04-27 03:00:24 -0500

    fix php errors again

--- header.php
+++ header.php
@@ -1,8 +1,6 @@
-echo '
-    <div id="menu">
+<div id="menu">
     <ul id="menulist">
     <li> <a href="games.html">Games</a> </li>
     <li> <a href="index.html">About</a> </li>
     </ul>
-    </div>
-';
+</div>
